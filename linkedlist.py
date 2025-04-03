1bc9a725f19628b22bcfcff4a3bc4e3433feae80
Steven-Minuk <minuk4688@gmail.com>
2025-04-03 10:19:22 -0500

    initial commit

---
+++ linkedlist.py
@@ -0,0 +1,180 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None: # or self.length == 0
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        
+        temp = self.head
+        pre = self.head
+
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+
+        if self.length == 0:
+           self.tail = None
+        
+        return temp
+    
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        
+        return temp # .value -> return the value 
+    
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        
+        if index == 0:
+            return self.prepend(value)
+        
+        if index == self.length:
+            return self.append(value)
+        
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+        
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        
+        if index == 0:
+            return self.pop_first()
+        
+        if index == self.length:
+            return self.pop()
+        
+        prev = self.get(index - 1)
+        temp = prev.next
+
+        prev.next = temp.next
+        temp = prev.next
+
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+    
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+
+        after = temp.next
+        before = None
+
+        for _ in range(self.length - 1):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+llist = LinkedList(4)
+llist.append(3)
+llist.append(2)
+llist.append(1)
+llist.print_list()
+print("------------------\n")
+
+llist.pop()
+llist.print_list()
+print("------------------\n")
+
+llist.prepend(5)
+llist.prepend(6)
+llist.prepend(7)
+llist.print_list()
+print("------------------\n")
+
+llist.pop_first()
+llist.print_list()
+
+print(llist.get(3))
+
+llist.set_value(3, 10)
+llist.print_list()
+print("------------------\n")
+
+llist.insert(3, 56)
+llist.print_list()
+print("------------------\n")
+
+llist.remove(1)
+llist.print_list()
+print("------------------\n")
+
+llist.reverse()
+llist.print_list()
+print("------------------\n")
