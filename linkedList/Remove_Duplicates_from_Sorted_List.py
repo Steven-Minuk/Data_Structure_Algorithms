16bb3e2540da7bca69f5e5ea48d0f1ef976a05e3
Minuk <minuk4688@gmail.com>
2025-07-31 02:29:59 -0500

    linkedlist

---
+++ linkedList/Remove_Duplicates_from_Sorted_List.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if head is None:
+            return None
+
+        prev = head
+        curr = head.next
+        while curr:
+            if prev.val == curr.val:
+                prev.next = curr.next
+                curr = curr.next
+
+            else:
+                prev = curr
+                curr = curr.next
+        return head
+                
